867f0cf1e12a5818e5da2bcef889fa4d702ee030
Burcu Noyan <burcunoyan@gmail.com>
2017-09-06 23:33:07 -0400

    dynamically create route methods and DRY

--- routes/days.js
+++ routes/days.js
@@ -32,88 +32,24 @@ app.delete('/:id', (req, res, next)=> {
     .catch(next);
 });
 
-app.post('/:dayId/hotels/:id', (req, res, next)=> {
-  Promise.all([
-      Day.findById(req.params.dayId),
-      Hotel.findById(req.params.id)
-    ])
-    .then(([day, hotel]) => {
-      return day.addHotel(hotel);
-    })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(next)
-});
+var pascalCase = require('pascal-case'); // to make the initial uppercase
 
-app.delete('/:dayId/hotels/:id', (req, res, next) => {
-  Promise.all([
-      Day.findById(req.params.dayId),
-      Hotel.findById(req.params.id)
-    ])
-    .then(([day, hotel]) => {
-      return day.removeHotel(hotel);
+[ Hotel, Restaurant, Activity ].forEach(model => {
+  [ 'post', 'delete' ].forEach(verb => {
+    app[verb](`/:dayId/${model.tableName}/:id`, (req, res, next) => {
+      Promise.all([
+        Day.findById(req.params.dayId),
+        model.findById(req.params.id)
+      ])
+      .then(([day, item]) => {
+        return day[`${verb === 'post' ? 'add' : 'remove'}${pascalCase(model.name)}`](item);
+      })
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch(next)
     })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(next);
-});
-
-app.post('/:dayId/restaurants/:id', (req, res, next)=> {
-  Promise.all([
-      Day.findById(req.params.dayId),
-      Restaurant.findById(req.params.id)
-    ])
-    .then(([day, restaurant]) => {
-      return day.addRestaurant(restaurant);
-    })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(next);
-});
-
-app.delete('/:dayId/restaurants/:id', (req, res, next)=> {
-  Promise.all([
-      Day.findById(req.params.dayId),
-      Restaurant.findById(req.params.id)
-    ])
-    .then(([day, restaurant]) => {
-      return day.removeRestaurant(restaurant);
-    })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(next);
-});
-
-app.post('/:dayId/activities/:id', (req, res, next) => {
-  Promise.all([
-      Day.findById(req.params.dayId),
-      Activity.findById(req.params.id)
-    ])
-    .then(([day, activity]) => {
-      return day.addActivity(activity);
-    })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(next);
-});
-
-app.delete('/:dayId/activities/:id', (req, res, next) => {
-  Promise.all([
-      Day.findById(req.params.dayId),
-      Activity.findById(req.params.id)
-    ])
-    .then(([day, activity]) => {
-      return day.removeActivity(activity);
-    })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(next);
+  });
 });
 
 module.exports = app;
